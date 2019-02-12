3df8bf6e820df23a6acedde56c3d4f42615318ba
salemxd <uzumaki.salem@gmail.com>
2019-02-13 00:01:33 +0100

    nästan klar!

--- public/js/deli-very.js
+++ public/js/deli-very.js
@@ -5,18 +5,9 @@
 var socket = io();
 
 var vm = new Vue({
-  el: '#dots',
+  el: '#vuebody',
   data: {
     orders: {},
-  },
-  created: function () {
-    socket.on('initialize', function (data) {
-      this.orders = data.orders;
-    }.bind(this));
-
-    socket.on('currentQueue', function (data) {
-      this.orders = data.orders;
-    }.bind(this));
   },
   methods: {
     getNext: function () {
@@ -26,18 +17,39 @@ var vm = new Vue({
       return lastOrder + 1;
     },
     addOrder: function (event) {
-      var offset = {x: event.currentTarget.getBoundingClientRect().left,
-                    y: event.currentTarget.getBoundingClientRect().top};
       socket.emit("addOrder", { orderId: this.getNext(),
-                                details: { x: event.clientX - 10 - offset.x,
-                                           y: event.clientY - 10 - offset.y },
-                                orderItems: ["Beans", "Curry"]
+                                details: this.orders[0].details,
+                                orderItems: this.orders[0].orderItems
                               });
     },
     displayOrder: function (){
       var offset = {x: event.currentTarget.getBoundingClientRect().left,
                     y: event.currentTarget.getBoundingClientRect().top};
-      Vue.set(this.orders, 0, {details: { x: event.clientX - 10 - offset.x, y: event.clientY - 10 - offset.y }});
+      var customerInformation = [
+        document.getElementById("Full name").value,
+        document.getElementById("E-mail").value,
+        document.getElementById("Payment method").value
+      ];
+      var radios = document.getElementsByName('genders');
+
+      if (document.getElementById("Male").checked) {
+        customerInformation.push(document.getElementById("Male").value);
+      }
+      if (document.getElementById("Female").checked) {
+        customerInformation.push(document.getElementById("Female").value);
+      }
+      if (document.getElementById("Non-binary").checked) {
+        customerInformation.push(document.getElementById("Non-binary").value);
+      }
+      if (document.getElementById("null").checked) {
+        customerInformation.push(document.getElementById("null").value);
+      }
+      for (var i = 0; i < menuitems.length; i++) {
+        if (document.getElementById(menuitems[i].name).checked) {
+          customerInformation.push(menuitems[i].name);
+        }
+      }
+      Vue.set(this.orders, 0, {details: { x: event.clientX - 10 - offset.x, y: event.clientY - 10 - offset.y }, orderItems: customerInformation.slice(4)});
     }
   }
 });
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,42 +1,7 @@
+
 var vm = new Vue({
   el: '#Hamburger-section',
   data: {
     menuitem: menuitems
   }
 })
-
-var vs = new Vue({
-    el: '#klar',
-    methods: {
-        markDone: function() {
-          var customerInformation = [
-            document.getElementById("Full name").value,
-            document.getElementById("E-mail").value,
-            document.getElementById("Payment method").value
-          ];
-          var radios = document.getElementsByName('genders');
-
-          if (document.getElementById("Male").checked) {
-            customerInformation.push(document.getElementById("Male").value);
-          }
-          if (document.getElementById("Female").checked) {
-            customerInformation.push(document.getElementById("Female").value);
-          }
-          if (document.getElementById("Non-binary").checked) {
-            customerInformation.push(document.getElementById("Non-binary").value);
-          }
-          if (document.getElementById("null").checked) {
-            customerInformation.push(document.getElementById("null").value);
-          }
-          for (var i = 0; i < menuitems.length; i++) {
-            if (document.getElementById(menuitems[i].name).checked) {
-              customerInformation.push(menuitems[i].name);
-            }
-          }
-
-          document.getElementById("order-information").innerHTML = "Din beställning:";
-          document.getElementById("burger-information").innerHTML = customerInformation.slice(4);
-          document.getElementById("important-information").innerHTML = customerInformation.slice(0,4);
-        }
-    }
-});
